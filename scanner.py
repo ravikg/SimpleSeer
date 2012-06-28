5616213c05e9e1291baea0414eb7ce33410585c9
Katherine Scott <kscottz@kscottz-laptop.(none)>
2012-06-28 11:43:04 -0400

    getting stuff in

--- scanner.py
+++ scanner.py
@@ -18,6 +18,47 @@ def waitforbuttons(state):
         if scan.device.email or scan.device.file or scan.device.copy or scan.device.dev.get_option(30):
             return state.core.state('scan')
 
+def straightenImg(img):
+    mask = img.threshold(20).dilate(2)
+    #TRY TO GET THE BOLD ALLIGNED RIGHT
+    #Try to figure out which side is most massive by color
+    UH = mask.crop(0,0,img.width,img.height/2).meanColor()[0]
+    BH = mask.crop(0,img.height/2,img.width,img.height/2).meanColor()[0]
+    RH = mask.crop(0,img.width/2,img.width/2,img.height).meanColor()[0]
+    LH = mask.crop(0,0,img.width/2,img.height).meanColor()[0]
+    sidethresh = 3
+    if( RH > sidethresh*LH ):
+        img = img.rotate(90,fixed=False)
+        mask = img.threshold(20).dilate(2)
+    elif( LH > sidethresh*RH):
+        img = img.rotate(-90,fixed=False)
+        mask = img.threshold(20).dilate(2)
+    
+    if( BH > UH ):
+        img = img.rotate(180,fixed=False)
+        mask = img.threshold(20).dilate(2)
+
+    b = img.findBlobsFromMask(mask,minsize=250)
+    outer = b[-1].mMask.edges()
+    lines = outer.findLines()
+    angles = lines.angle()
+    #go through our lines and pick the near vertical and 
+    # horizontal lines that have the greatest numbers of samples 
+    a = 30
+    testhp = angles[(angles<a)&(angles>0)]
+    testhn = angles[(angles>-1*a)&(angles<0)]
+    a = 70
+    testvp = -1*(90-angles[(angles>a)])
+    testvn = -1*(-90-angles[(angles<-1*a)])
+    values = [testhp,testhn,testvp,testvn]
+    #get the one with the most values
+    counts = [len(testhp),len(testhn),len(testvp),len(testvn)]
+    best = np.argmax(counts)
+    #take the median of these to filter out outliers
+    final_rotation = np.median(values[best])
+    img = img.rotate(final_rotation,fixed=False)        
+    return img
+
 @core.state('scan')
 def scan(state):
     core = state.core
@@ -68,12 +109,18 @@ def scan(state):
                 channels = np.where(stripe.min(0) > 10)
                 for channel in channels:
                     nump[index,:,channel] = np.round(np.mean([nump[index-1,:,channel], nump[index+1,:,channel]], 0))
-        frame.image = Image(nump)
+        
+        #now straigten out the image
+        temp = Image(nump)
+        temp = straightenImg(temp)
+        frame.image = temp
                
         process(frame)
 	t = frame.thumbnail
         frame.save()
         id = frame.id
+        
+
     scan.setROI()
     M.Alert.clear()
     M.Alert.redirect("frame/" + str(id))
