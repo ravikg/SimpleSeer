21ffe38bcb7872a4b5012a509112028c8c0f4730
Katherine Scott <kscottz@kscottz-laptop.(none)>
2012-07-10 13:51:01 -0400

    okay this doesn't work quite right. going to try a region based approach

--- SimpleSeer/plugins/Fastener/grain.py
+++ SimpleSeer/plugins/Fastener/grain.py
@@ -10,25 +10,21 @@ def scanner_preprocess(img):
 
 def maxInWin(slice,ic,win):
     result = []
-    w = len(slice)-5
+    w = len(slice)-2
     for sp in ic:
-        ub = np.clip(sp+win,0,len(slice)-1)
-        lb =np.clip(sp-win,0,len(slice)-1)
-        mv = np.max(slice[lb:ub])
-
-        if(mv == 0):
-            result.append(np.clip(sp,0,w))
+        ub = np.clip(sp+win,0,len(slice))
+        lb =np.clip(sp-win,0,len(slice))
+        mv = np.min(slice[lb:ub])
+        loc = np.where(slice[lb:ub]==mv)[0]
+        if(len(loc) == 1 ):
+            result.append(np.clip(loc[0]+lb,0,w))
         else:
-            loc = np.where(slice[lb:ub]==mv)[0]
-            if(len(loc) == 1 ):
-                result.append(np.clip(loc[0]+lb,0,w))
-            else:
-                temp = (loc-sp)**2
-                idx = np.where(temp==np.min(temp))[0]
-                result.append(np.clip(loc[idx][0]+lb,0,w))
+            temp = (loc-(win))**2
+            idx = np.where(temp==np.min(temp))[0]
+            result.append(np.clip(loc[idx][0]+lb,0,w))
     return result
 
-def getGrains(img,seeds=20,win=10):
+def getGrains(img,seeds=40,win=5):
     retVal = []
     seedpts = np.floor(np.linspace(0,img.width,seeds))
     seedpts = maxInWin(img.getHorzScanlineGray(0),seedpts,win)
@@ -49,7 +45,6 @@ def getGrains(img,seeds=20,win=10):
         for ds in d:
             img.drawLine(last,ds,color=Color.RED,thickness=2)
             last = ds
-            #img[np.floor(ds[0]),np.floor(ds[1])]=Color.RED
         vectors.append(d)
 
     img.applyLayers()
@@ -69,10 +64,10 @@ for p in path:
         grain = b[-1].blobImage()
         grain = grain.crop(raw.width*(3/16.0),raw.height/8,raw.width*(9/16.0),raw.height/3)
         rawData = raw.crop(raw.width*(3/16.0),raw.height/8,raw.width*(9/16.0),raw.height/3)
-        rawData = rawData.equalize()
+        rawData = rawData
         temp = Image((rawData.width,rawData.height))
-        temp = temp.blit(rawData,mask=grain)
-        result = getGrains(temp)
+        temp = temp.blit(rawData,mask=grain).smooth().smooth(aperature=15)
+        result = getGrains(rawData.equalize())
         result.show()
         result.save(fname)
 
