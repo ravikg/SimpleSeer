f1cc8fa7d2535ddc597cbafbd772b4069f29c2a6
brooksryba <brooks@sightmachine.com>
2012-08-29 10:20:21 -0400

    Fixed toLocateDate issue

--- SimpleSeer/static/vendor/scripts/sm.datetimerange.js
+++ SimpleSeer/static/vendor/scripts/sm.datetimerange.js
@@ -15,7 +15,8 @@ SimpleSeerDateHelper = {
     },
     
     prettyDate: function(date) {
-        return date.toLocaleDateString().match(/\w+\s\d+\,\s\d+/)[0];
+        var str = [this.monthNames[date.getMonth()], " ", date.getDate(), ", ", date.getFullYear()];
+	return str.join("");
     },
     
     prettyTime: function(d) {
