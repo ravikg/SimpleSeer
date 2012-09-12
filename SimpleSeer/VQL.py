4d05c471df2b224377da1429a898aee6477ddc0c
Kurt DeMaagd <kurt@ingenuitas.com>
2012-09-12 18:16:45 -0400

    Add support for groupings with { }

--- SimpleSeer/VQL.py
+++ SimpleSeer/VQL.py
@@ -13,43 +13,44 @@ class VQL:
         g = VQL.grammar()
         
         try:
-            multiple = g.parseString(query)
+            parsed = g.parseString(query)
         except ParseException, e:
             return "Parse Error, line %s, col %s" % (e.loc, e.column), 500
         
-        for single in multiple:
-            inspection = single[0]
-            measurements = single[1]
+        for group in parsed:
+            for single in group:
+                inspection = single[0]
+                measurements = single[1]
+                
+                insp = M.Inspection()
+                inspMethod = inspection[0]
+                insp.name = inspMethod
+                
+                if not inspMethod in insp.register_plugins('seer.plugins.inspection'):
+                    return "Unknown method: %s" % inspMethod, 500
+                insp.method = inspMethod
+                
+                if len(inspection) > 1:
+                    inspParams = {}
+                    for p in inspection[1]:
+                        if len(p) > 1:
+                            inspParams[p[0]] = p[1]
+                        else:
+                            plugin = insp.get_plugin(insp.method)
+                            reverse = plugin.reverseParams()
+                            if p[0] not in reverse:
+                                return "Unrecognized shortcut parameter: %s" % p[0], 500
+                            inspParams[reverse[p[0]]] = p[0]
+                    insp.parameters = inspParams
+                insp.save()
+                
+                for m in measurements:
+                    meas = M.Measurement()
+                    meas.name = m
+                    meas.method = m
+                    meas.inspection = insp.id
+                    meas.save()
             
-            insp = M.Inspection()
-            inspMethod = inspection[0]
-            insp.name = inspMethod
-            
-            if not inspMethod in insp.register_plugins('seer.plugins.inspection'):
-                return "Unknown method: %s" % inspMethod, 500
-            insp.method = inspMethod
-            
-            if len(inspection) > 1:
-                inspParams = {}
-                for p in inspection[1]:
-                    if len(p) > 1:
-                        inspParams[p[0]] = p[1]
-                    else:
-                        plugin = insp.get_plugin(insp.method)
-                        reverse = plugin.reverseParams()
-                        if p[0] not in reverse:
-                            return "Unrecognized shortcut parameter: %s" % p[0], 500
-                        inspParams[reverse[p[0]]] = p[0]
-                insp.parameters = inspParams
-            insp.save()
-            
-            for m in measurements:
-                meas = M.Measurement()
-                meas.name = m
-                meas.method = m
-                meas.inspection = insp.id
-                meas.save()
-        
         return "YAY", 200
     
     @classmethod
@@ -80,4 +81,6 @@ class VQL:
         singleQuery = Group(Group(inspection) +  Group(Optional(measurement)))
         multiQuery = OneOrMore(singleQuery)
 
-        return multiQuery
+        groupedQuery = Optional(Group(multiQuery)) + ZeroOrMore(Group(Suppress("{") + multiQuery + Suppress("}"))) + Optional(Group(multiQuery))
+
+        return groupedQuery
