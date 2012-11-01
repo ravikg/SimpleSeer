75819d054c6fdc8b0d833369abaf6a70151fafa4
Kurt DeMaagd <kurt@ingenuitas.com>
2012-11-01 12:21:53 -0400

    do not convert metadata to numeric

--- SimpleSeer/Filter.py
+++ SimpleSeer/Filter.py
@@ -212,7 +212,7 @@ class Filter():
             if 'eq' in f:
                 
                 # Need to convert the numbers into digits instead of strings
-                if (type(f['eq']) == str or type(f['eq']) == unicode) and f['eq'].isdigit():
+                if (type(f['eq']) == str or type(f['eq']) == unicode) and f['eq'].isdigit() and not nameParts[-1] == 'metadata':
                     f['eq'] = float(f['eq'])
                 
                 # Convert datetimes into epoch ms
