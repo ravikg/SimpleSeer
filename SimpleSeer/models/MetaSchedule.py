5b99880162328fa0df4efb983182be2b2bd48223
Kurt DeMaagd <kurt@archie.(none)>
2012-12-07 12:43:21 -0500

    renamed file

--- SimpleSeer/models/MetaSchedule.py
+++ SimpleSeer/models/MetaSchedule.py
@@ -2,6 +2,7 @@ import pymongo
 from collections import defaultdict
 
 from . import Frame, Measurement
+from ..worker import backfill_tolerances
 
 class Foreman():
     
@@ -10,21 +11,22 @@ class Foreman():
     def __init__(self):
         self._db = Frame._get_db()
         
-    def enqueue(self, measurement_id):
+    def enqueue_measurement(self, measurement_id):
         for f in Frame.objects:
             # Add the measurement to the frame/measurement grid.  Create the entry if it does not exist
             self._db.foreman.update({'frame_id': f.id}, {'$push': {'measurements': measurement_id}}, True)
+    
+    def enqueue_inspection(self, inspection_id):
+        for f in Frame.objects:
+            self._db.foreman.update({'frame_id': f.id}, {'$push': {'inspections': inspection_id}}, True)
     
     def run(self):
         
         res = self._db.foreman.find_and_modify({}, remove=True)
         while res:
-            f = Frame.objects.get(id=res['frame_id'])
-            
-            for m_id in res['measurements']:
-                m = Measurement.objects.get(id=m_id)
-                m.execute(f, f.features)
-            
+            f = Frame.objects.get(res['frame_id'])
+            f.results = backfill_tolerances.delay(res['measurements'], f.id)
             f.save()
+            
             res = self._db.foreman.find_and_modify({}, remove=True)
         
