35dfe9090f9b608dab36def0241ed096f1aa6c46
Brooks Ryba <brooks@sightmachine.com>
2013-02-22 10:26:56 -0500

    Updated my jquery plugin to give out the percentage scrolled along each step of the way

--- SimpleSeer/static/vendor/scripts/jquery.infiniteScroll.js
+++ SimpleSeer/static/vendor/scripts/jquery.infiniteScroll.js
@@ -1,7 +1,7 @@
 (function($) {
 	$.fn.infiniteScroll = function(options) {
 		var defaults = {
-			wrapper: "asb-wrapper",
+			wrapper: "infs-wrapper",
 			callback: function() {}
 		}
 
@@ -10,16 +10,18 @@
 		return this.each(function(idx, obj) {
 			$(obj).wrapInner('<div class="'+defaults.wrapper+'" />');
 			$(obj).on('scroll', function(e) {
-				self = $(e.target);
-				selfHeight = self.height();
-				selfScroll = self.scrollTop();
-				wrapperHeight = self.find("." + defaults.wrapper).height();
+				var percent = 0;
+				var self = $(e.target);
+				var selfHeight = self.height();
+				var selfScroll = self.scrollTop();
+				var wrapperHeight = self.find("." + defaults.wrapper).height();
 
-				atBottom = wrapperHeight - selfHeight <= selfScroll;
-				if( atBottom ) {
-					defaults.callback()							
-				}
-			})
+				if( selfScroll == 0 ) { percent = 0; }
+				else if( wrapperHeight - selfHeight <= selfScroll ) { percent = 100; }
+				else { percent = (selfScroll) / (wrapperHeight - selfHeight); percent = Math.ceil(percent * 100); }
+				
+				defaults.callback(percent)							
+			});
 		});
 	}
 })(jQuery);
