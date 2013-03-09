7b3bee46d1e11f015e85af70773422fa60e00234
Tendrid <tendrid@gmail.com>
2013-03-09 01:59:35 -0500

    - finished context - added lifespan (auto kill) to tab containers - made re-renders safe for tab/dashboard structure - slight revamp to menus - eliminated several superfluous DOM nodes

--- SimpleSeer/models/Context.py
+++ SimpleSeer/models/Context.py
@@ -3,21 +3,23 @@ from formencode import validators as fev
 from formencode import schema as fes
 #import formencode as fe
 
-#from SimpleSeer import validators as V
+from SimpleSeer import validators as V
 #from SimpleSeer import util
 
 from SimpleSeer.models.base import SimpleDoc
 
 class ContextSchema(fes.Schema):
     name = fev.UnicodeString(not_empty=True)
+    options = V.JSON(if_empty={}, if_missing={})
     menuItems = fev.Set(if_empty=[], if_missing=[])
 
 class Context(SimpleDoc, mongoengine.Document):
     name = mongoengine.StringField(default='')
+    options = mongoengine.DictField()
     menuItems = mongoengine.ListField(mongoengine.DictField())
 
     def __repr__(self):
-        return "[%s Object <%s> ]" % (self.__class__.__name__, self.name)
+        return "%s Object <%s>" % (self.__class__.__name__, self.name)
         
     def __eq__(self, other):
         if isinstance(other, self.__class__):
