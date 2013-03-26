747054b0708b61598354acc501b36ae435978d3b
Tendrid <tendrid@gmail.com>
2013-03-26 11:31:27 -0400

    new motion meas

--- SimpleSeer/plugins/Motion/motion.py
+++ SimpleSeer/plugins/Motion/motion.py
@@ -17,11 +17,31 @@ meas.save()
 
 """
 
+class MotionTrend(base.MeasurementPlugin):
+    
+    def __call__(self, frame, featureset):
+        meas = self.measurement
+        timeframe = meas.parameters.get("timeframe", 0)
+        motionthreshhold = meas.parameters.get("motionthreshhold", 0)
+        
+        frameset = Frame.objects(capturetime__gt = frame.capturetime - timedelta(0, timewindow),
+           capturetime__lt = frame.capturetime, 
+           camera = frame.camera
+           ).order_by("capturetime")
+        if len(frameset) < minframes:
+            return []
+        
+        frameset = list(frameset) #load into memory
+
+
+
+
 class MotionFeature(SimpleCV.Feature):
   movement = 0.0
 
   def __init__(self, image, mval, compared_with=None, top = 0, left = 0, right = -1, bottom = -1):
-    #TODO, if parameters are given, crop on both images
+    #TODO, if parameters are given, crop on both images    
+
     self.image = image
     self.movement = mval
     self.compared = compared_with
