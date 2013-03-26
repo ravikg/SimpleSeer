9fca558704dff88de6062df8cb0d1087dc1b85be
Kurt DeMaagd <kurt@sightmachine.com>
2013-03-25 20:06:06 -0400

    Point celery to amqp

--- SimpleSeer/celeryconfig.py
+++ SimpleSeer/celeryconfig.py
@@ -2,13 +2,14 @@ from .Session import Session
 
 session = Session(".")
 
-BROKER_URL = 'mongodb://%s:%d/%s' % (session.mongo['host'], session.mongo['port'], session.database)
+#BROKER_URL = 'mongodb://%s:%d/%s' % (session.mongo['host'], session.mongo['port'], session.database)
+BROKER_URL = 'amqp://%s//' % session.rabbitmq
 CELERY_IMPORTS = ("SimpleSeer.worker",)
-CELERY_RESULT_BACKEND = "mongodb"
-CELERY_MONGODB_BACKEND_SETTINGS = dict(
-   host = session.mongo['host'],
-   port = int(session.mongo['port']),
-   database = session.database,
-   taskmeta_collection = "celery_taskmeta",
-   user = '',
-   password = '')
+#CELERY_RESULT_BACKEND = "mongodb"
+#CELERY_MONGODB_BACKEND_SETTINGS = dict(
+#   host = session.mongo['host'],
+#   port = int(session.mongo['port']),
+#   database = session.database,
+#   taskmeta_collection = "celery_taskmeta",
+#   user = '',
+#   password = '')
