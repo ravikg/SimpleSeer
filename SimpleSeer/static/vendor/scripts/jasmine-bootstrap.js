3d92592f5ec30c870b1d03a179531adc33f706e3
Brooks Ryba <brooks@sightmachine.com>
2013-06-11 14:56:40 -0400

    Removed old test bootstrap.

--- SimpleSeer/static/vendor/scripts/jasmine-bootstrap.js
+++
@@ -1,9 +0,0 @@
-window.runTest = (function() {
-	var jasmineEnv = jasmine.getEnv();
-	jasmineEnv.updateInterval = 1000;
-
-	var htmlReporter = new jasmine.HtmlReporter();
-	jasmineEnv.addReporter(htmlReporter);
-	jasmineEnv.specFilter = function(spec) { return htmlReporter.specFilter(spec) }
-	jasmineEnv.execute();
-})
