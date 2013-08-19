f2daa56ece49d7fbf980cb7fd69414fbd6f19f9e
Tendrid <tendrid@gmail.com>
2013-08-19 15:32:10 -0400

    finalize

--- SimpleSeer/tests/tools/seer.py
+++ SimpleSeer/tests/tools/seer.py
@@ -1,10 +1,7 @@
-import mongoengine
 import time
 from multiprocessing import Process
 from SimpleSeer.commands import core_commands
 from SeerCloud.commands import olap
-
-
 
 class SeerInstanceTools(object):
     seer_types = {
@@ -15,34 +12,26 @@ class SeerInstanceTools(object):
     seer_instance = {}
 
     def killall_seer(self):
-        for i,o in self.seer_instance.iteritems():
-            print "killing {0}".format(i)
-            o.terminate()
+        for key in self.seer_instance.keys():
+            self.kill_seer(key)
 
+    def kill_seer(self,instance):
+        print "killing {0}".format(instance)
+        self.seer_instance[instance].terminate()
+        del self.seer_instance[instance]
 
     def spinup_seer(self,type,config='./config/test_simpleseer.cfg',config_override={}):
         import argparse
-        from SimpleSeer.Session import Session
         parser = argparse.ArgumentParser()
         parser.add_argument('-c', '--config', dest='config', default='.')
-        #ss = core_commands.WebCommand(parser)
         ss = self.seer_types[type](parser)
         options = parser.parse_args()
         options.config = "./config/"
         ss.configure(options)
         for k,v in config_override.iteritems():
-        	print k,v
-        	setattr(ss, k, v)
+            setattr(ss, k, v)
 
-        #print self.mongo_settings
         time.sleep(20)
-        #print "====================================== RUN MONGO TESTS NOW ====================================================="
-        #time.sleep(60)
-        #mongoengine.connection.connect('test', **self.mongo_settings)
-        #ss.session.mongo = self.mongo_settings
-        
-        #time.sleep(10)
         self.seer_instance[type] = Process(target=ss.run)
         self.seer_instance[type].start()
         time.sleep(3)
-        #print self.seer_instance[type].communicate()
